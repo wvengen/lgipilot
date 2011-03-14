36ef1b9e0c70255ba51ffe95d1c6a3bd1908f1db
wvengen <wvengen@nikhef.nl>
2011-03-14 15:19:16 +0100

    add automatic modification of resource daemon scripts for auto-termination

--- pilotdist/pilotscript_autoterm.sh
+++ pilotdist/pilotscript_autoterm.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-[ "${LGI_IS_PILOTJOB}" ] || exit 0
-NOW=`date +%s`
+if [ "${LGI_IS_PILOTJOB}" ]; then
+	NOW=`date +%s`
 
-# kill daemon when idle time exceeds predefined value
-if [ -e "${LGI_RUNNING_STAMP}" -a "${SCHED_WAIT_TERM}" ]; then
-	LASTRUNNING=`cat "${LGI_RUNNING_STAMP}"`
-	DIFF=`expr "${NOW}" - "${LASTRUNNING}"`
-	if [ ${DIFF} -ge ${SCHED_WAIT_TERM} ]; then
-		echo "pilotjob: killing daemon after ${DIFF} seconds of idle-time"
-		kill `cat ${LGI_PID_FILE}`
+	# kill daemon when idle time exceeds predefined value
+	if [ -e "${LGI_RUNNING_STAMP}" -a "${SCHED_WAIT_TERM}" ]; then
+		LASTRUNNING=`cat "${LGI_RUNNING_STAMP}"`
+		DIFF=`expr "${NOW}" - "${LASTRUNNING}"`
+		if [ ${DIFF} -ge ${SCHED_WAIT_TERM} ]; then
+			echo "pilotjob: killing daemon after ${DIFF} seconds of idle-time"
+			kill `cat ${LGI_PID_FILE}`
+		fi
+	fi
+
+	# kill daemon when run-time exceeds predefined value
+	if [ -e "${LGI_START_STAMP}" -a "${SCHED_TERM_AFTER}" ]; then
+		START=`cat "${LGI_START_STAMP}"`
+		DIFF=`expr "${NOW}" - "${START}"`
+		if [ ${DIFF} -ge ${SCHED_TERM_AFTER} ]; then
+			echo "pilotjob: killing daemon after ${DIFF} seconds of run-time"
+			kill `cat ${LGI_PID_FILE}`
+		fi
 	fi
 fi
-
-# kill daemon when run-time exceeds predefined value
-if [ -e "${LGI_START_STAMP}" -a "${SCHED_TERM_AFTER}" ]; then
-	START=`cat "${LGI_START_STAMP}"`
-	DIFF=`expr "${NOW}" - "${START}"`
-	if [ ${DIFF} -ge ${SCHED_TERM_AFTER} ]; then
-		echo "pilotjob: killing daemon after ${DIFF} seconds of run-time"
-		kill `cat ${LGI_PID_FILE}`
-	fi
-fi
-
--- pilotdist/pilotscript_touch.sh
+++ pilotdist/pilotscript_touch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-[ "${LGI_IS_PILOTJOB}" ] || exit 0
-# keep track of non-idle time
-date "+%s" >"${LGI_RUNNING_STAMP}"
+if [ "${LGI_IS_PILOTJOB}" ]; then
+	# keep track of non-idle time
+	date "+%s" >"${LGI_RUNNING_STAMP}"
+fi
