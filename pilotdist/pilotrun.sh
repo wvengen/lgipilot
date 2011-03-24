8fd65d4f8a2eabb0c3a5109046a4f205d4e08b0e
wvengen <wvengen@nikhef.nl>
2011-03-24 15:55:35 +0100

    some more copyright headers

--- pilotdist/pilotrun.sh
+++ pilotdist/pilotrun.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 #
-# Run the LGI resource daemon
+# LGI pilot job resource daemon run script
+#   unpacks the pilot job tarball and sets up auto-termination environment 
 #
+# Copyright (C) 2011 W. van Engen, Stichting FOM, The Netherlands
+#
+# This is free software; you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation.
+#
+# http://www.gnu.org/licenses/gpl.txt
 
 # extract LGI resource daemon and application
 tar xzf pilotjob.tar.gz
